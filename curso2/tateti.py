9916f2ec0e33fc67cf009311078897c7a729e593
Igna <gnachoxp@gmail.com>
2020-10-31 13:09:42 -0300

    end tateti

--- curso2/tateti.py
+++ curso2/tateti.py
@@ -1,115 +1,106 @@
-# Se va corroborando si hay un ganador
-# El juego termina con empate cuando se llena el tablero
-
-
-#### MIS EXCEPCIONES:
-class OutOfTable(Exception):
-   message = "El campo {field} no es valido"
-   def __init__(self, field: str) -> None:
-      self.field = field
-      super().__init__(self.message.format(field=field))
-
-class InvalidField(Exception):
-   message = "El campo {field} ya se encuentra ocupado"
-   def __init__(self, field: str) -> None:
-      self.field = field
-      super().__init__(self.message.format(field=field))
-###
+from os import system, name
 
 ### FUNCION PARA PINTAR MATRIZ:
 def printTable(matriz):
    print('-  0 1 2 ')
    for i in range(3):
-      print('{} |{}|{}|{}|'.format(i,matriz[i][0],matriz[i][1],matriz[i][2]))
+      print('{}  {}|{}|{}'.format(i,matriz[i][0],matriz[i][1],matriz[i][2]))
 ###
 
 ### FUNCION PARA ELEGIR CAMPO:
 def choiceField(player):
    print("Jugador {}".format(player))
-   row = int(input("En que fila colocara su figura? : "))
+   row = int(input("\nEn que fila colocara su figura? : "))
    column = int(input("En que columna colocara su figura? : ")) 
    return [row,column]
 ###
 
-### FUNCION PARA CHEQUEAR QUE NO HALLA EXCEPTION OutOfTable
-def checkOutOfTable(move):
-   if move[0] > 2 or move[0] < 0 or move[0] > 2 or move[1] < 0:
-      raise OutOfTable("[{}][{}]".format(move[0],move[1]))
+### FUNCION PARA CHEQUEAR QUE NO HALLA EXCEPTION
+def checkInvalidField(table, move):
+   if move[0]>2 or move[0]<0 or move[0]>2 or move[1]<0 or table[move[0]][move[1]]=="O" or table[move[0]][move[1]]=="X":
+      return True
+   return False
 ###
 
-### FUNCION PARA CHEQUEAR QUE NO HALLA EXCEPTION InvalidField
-def checkInvalidField(move, table):
-   if table[move[0]][move[1]]=="O" or table[move[0]][move[1]]=="X":
-      raise OutOfTable("[{}][{}]".format(move[0],move[1]))
+### FUNCION PARA CAMBIAR DE JUGADOR
+def togglePlayer(flag):
+   if flag:
+      return "X", not flag
+   return "O", not flag
 ###
 
 ### FUNCION PARA CHEQUEAR SI TERMINO EL JUEGO
 def checkEnd(table):
-   if table[0][0] == table[1][1] == table[2][2] != "_" or table[0][2] == table[1][1] == table [2][0] != "_" :
+   if table[0][0] == table[1][1] == table[2][2] != " " or table[0][2] == table[1][1] == table [2][0] != " " :
       return True
    else:
       for x in range(3):
-         if table[0][x] == table[1][x] == table[2][x] != "_" or table[x][0] == table[x][1] == table[x][2] != "_":
+         if table[0][x] == table[1][x] == table[2][x] != " " or table[x][0] == table[x][1] == table[x][2] != " ":
             return True
    return False
 ###
 
-### MAIN:
-table = [
-   ["_","_","_"],
-   ["_","_","_"],
-   ["_","_","_"]
-]
-printTable(table)
+### FUNCION PARA CHEQUEAR QUIEN GANO
+def checkWinner(x):
+   if x % 2 == 0:
+      return "X"
+   return "O"
+###
 
-choice= bool(input("Seleccione su figura: \n0- O\n1- X\n: "))
-if choice:
-   player1= "X"
-   player2= "O"
-else:
-   player1= "O"
-   player2= "X"
+### FUNCION CLEARSCREEN
+def clear(): 
+   if name == 'nt': 
+      _ = system('cls') 
+   else: 
+      _ = system('clear') 
+###
 
-try:
-   flag= choice
+### FUNCION MAIN:
+def main():
+   table = [
+      [" "," "," "],
+      [" "," "," "],
+      [" "," "," "]  
+   ]
+   printTable(table)
+
+   input("\nSeleccione su figura: \n0- O\n1- X\n: ")
+
+   flag= True
    end= 0
+
    for x in range(9):
-      if flag:
-         move = choiceField(1)
+      print("\nQuedan {} movimientos posibles!!!".format(9-x))
+      player, flag= togglePlayer(flag)
 
-         checkOutOfTable(move)
-         checkInvalidField(move, table)
+      move = choiceField(player)
 
-         table[move[0]][move[1]]= player1
-         printTable(table)
-         flag= not flag
+      notValid = checkInvalidField(table, move)
+      while notValid:
+         print("El campo [{}] [{}] no existe o se encuentra ocupado".format(move[0],move[1]))
+         move = choiceField(player)
+         notValid = checkInvalidField(table, move)
 
-      else:
-         move = choiceField(2)
 
-         checkOutOfTable(move)
-         checkInvalidField(move, table)
-
-         table[move[0]][move[1]]= player2
-         printTable(table)
-         flag= not flag
+      table[move[0]][move[1]]= player
+      
+      clear()
+      printTable(table)
 
       end= checkEnd(table)
       if end:
          print("Hay un ganador!")
+         winner = checkWinner(x)
          break
-   pass
 
-except (InvalidField, OutOfTable) as err:
-   print(err)
-   pass
-
-finally:
    if end:
-      print("Quien ha ganado es ...")
-      ###CALCULAR GANADOR
+      print("Quien ha ganado es {}".format(winner))
    else:
       print("El juego termino en empate")
-   pass
-
 ###
+
+play = True
+while play:
+   clear()
+   main()
+   play= int(input("\nDesea seguir jugando? \n0: NO\n1: SI\n:"))
